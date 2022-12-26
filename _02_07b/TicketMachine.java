35d315f4c5ee72cc300e69f5a04d06e373d2b221
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-12-26 00:50:13 +0000

    completed ticketMachine challenge, added object

--- _02_07b/TicketMachine.java
+++ _02_07b/TicketMachine.java
@@ -1,23 +1,35 @@
 package _02_07b;
 
 public class TicketMachine {
-  
-public static void main(String[] args) {
-  // Create an object called ticket of type Ticket
 
-  // Set the destination of the ticket to New York
+  public static void main(String[] args) {
+    // Create an object called ticket of type Ticket
 
-  // Set the price of the ticket to 15.30
+    Ticket ticket = new Ticket();
 
-  // Set the isReturn value to true
+    // Set the destination of the ticket to New York
+    ticket.setDestination("New York");
 
-  // Print the ticket's destination to the console
+    // Set the price of the ticket to 15.30
 
-  // Print the ticket's price to the console
+    ticket.setPrice(15.30);
 
-  // Print the ticket's isReturn value to the console
+    // Set the isReturn value to true
 
-}
+    ticket.setIsReturn(true);
 
+    // Print the ticket's destination to the console
+
+    System.out.println(ticket.getDestination());
+
+    // Print the ticket's price to the console
+
+    System.out.println(ticket.getPrice());
+
+    // Print the ticket's isReturn value to the console
+
+    System.out.println(ticket.getIsReturn());
+
+  }
 
 }
