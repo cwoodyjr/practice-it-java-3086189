7f70d0cb62640f906a74883e8c3192c27ccbf657
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2022-12-26 00:07:40 +0000

    completed menu builder challenge, Added items to array list and printed items as an array list called menu

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -18,18 +18,31 @@ public class MenuBuilder {
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
 
+    MenuItem starter = new MenuItem("Prawn Cocktail");
+
     // Add the starter variable to the ArrayList called menu.
+    menu.add(starter);
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
 
+    MenuItem mainCourse = new MenuItem("T-Bone Steak");
+
     // Add the mainCourse variable to the ArrayList called menu.
+
+    menu.add(mainCourse);
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
 
+    MenuItem dessert = new MenuItem("Spotted Dick With Custard");
+
     // Add the dessert variable to the ArrayList called menu.
 
+    menu.add(dessert);
+
     // Print the menu variable to the console.
+
+    System.out.println(menu);
   }
 }
